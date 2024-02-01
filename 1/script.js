b0047f7b8404b76a69f88e314c40a937453b4b48
TrapjawwS <145371179+TrapjawwS@users.noreply.github.com>
2024-02-01 09:12:36 -0800

    Update script.js

--- 1/script.js
+++ 1/script.js
@@ -6,5 +6,5 @@ if (notpirated === "false") {
 } else {
     alert("Hi. Before you start playing, I just want to remind you that I have 1 rule: DO NOT SHARE THIS FILE WITH OTHER PEOPLE. If you do, I can remote disable the game for you, and anybody you share this with. (and no refunds)")
     alert("also, if you can't figure out the controls, wiggle your mouse and click the controller icon in the bottom left to see and modify the controls. Have fun!")
-    alert("also, only use this game when given explicit permission to play games by your teacher (this is my disclaimer, so if a teacher asks me about this, there is no reason I coukd get int trouble. Sorry I keep yapping, just go play the game.")
+    alert("also, only use this game when given explicit permission to play games by your teacher (this is my disclaimer, so if a teacher asks me about this, there is no reason I coukd get int trouble.) Sorry I keep yapping, just go play the game.")
 }
