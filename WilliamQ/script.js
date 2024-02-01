0d060991b6fb4d08fbf0bf5944298f22ecf0a42c
thisismyaltaccountt <156016767+thisismyaltaccountt@users.noreply.github.com>
2024-02-01 03:01:09 +0000

    added william's stuff

---
+++ WilliamQ/script.js
@@ -0,0 +1,6 @@
+notpirated = "true";
+
+if (notpirated === "false") {
+    alert("Don't be a stinky pirate, stop sharing files with each other");
+    window.location.replace("http://google.com");
+}
--- script.js
+++
@@ -1,6 +0,0 @@
-notpirated = "false";
-
-if (notpirated === "false") {
-    alert("Don't be a stinky pirate, how about you ask me first >:(");
-    window.location.replace("http://google.com");
-}
