eebb019babef899d8e9b329132b3f1fa7461d22a
TrapjawwS <145371179+TrapjawwS@users.noreply.github.com>
2024-02-02 23:28:42 +0000

    added games

--- 2/script.js
+++ 2/script.js
@@ -4,7 +4,7 @@ if (notpirated === "false") {
     alert("Don't be a stinky pirate, stop sharing files with each other");
     window.location.replace("http://google.com");
 } else {
-    alert("By clicking OK you accept that I am not responsible for your actions, and that you will follow the chromebbok responsible use thing")
+    alert("By clicking OK you accept that I am not responsible for your actios, and that you will follow the chromebbok responsible use thing")
     alert("Hi. Before you start playing, I just want to remind you that I have 1 rule: DO NOT SHARE THIS FILE WITH OTHER PEOPLE. If you do, I can remote disable the game for you, and anybody you share this with. (and no refunds)")
     alert("also, if you can't figure out the controls, wiggle your mouse and click the controller icon in the bottom left to see and modify the controls. Have fun!")
 }
